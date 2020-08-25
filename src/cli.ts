69ba4879483f6d7f15d505e89b9acbd41a9224ec
Tiago Pascoal <tiago@pascoal.net>
2020-08-25 13:41:49 +0100

    Add codeql rule suites (#56)
    * Add codeql rule suite
    
    * Fix yaml warning on config file
    
    * fix lint warnings
    
    * restrict push and prs to master

--- src/cli.ts
+++ src/cli.ts
@@ -1,89 +1,89 @@
 #!/usr/bin/env node
 
-import * as common from './common';
-import * as nodeApi from 'azure-devops-node-api';
+import * as common from './common'
+import * as nodeApi from 'azure-devops-node-api'
 
-import * as WorkItemTrackingApi from 'azure-devops-node-api/WorkItemTrackingApi';
+import * as WorkItemTrackingApi from 'azure-devops-node-api/WorkItemTrackingApi'
 
-import * as CoreApi from 'azure-devops-node-api/CoreApi';
-import * as WorkItemTrackingInterfaces from 'azure-devops-node-api/interfaces/WorkItemTrackingInterfaces';
-import * as CoreInterfaces from 'azure-devops-node-api/interfaces/CoreInterfaces';
+import * as CoreApi from 'azure-devops-node-api/CoreApi'
+import * as WorkItemTrackingInterfaces from 'azure-devops-node-api/interfaces/WorkItemTrackingInterfaces'
+import * as CoreInterfaces from 'azure-devops-node-api/interfaces/CoreInterfaces'
 
 import { JsonPatchDocument, JsonPatchOperation, Operation } from "azure-devops-node-api/interfaces/common/VSSInterfaces"
 
 
-import Colors = require('colors/safe');
-import yargs = require('yargs');
+import Colors = require('colors/safe')
+import yargs = require('yargs')
 
 async function getQuery(witApi: WorkItemTrackingApi.IWorkItemTrackingApi, project: CoreInterfaces.TeamProject, queryName: string) {
-    common.heading(`Getting query ${queryName}`);
+    common.heading(`Getting query ${queryName}`)
 
-    const query = await witApi.getQuery(project.id, queryName, WorkItemTrackingInterfaces.QueryExpand.Wiql);
+    const query = await witApi.getQuery(project.id, queryName, WorkItemTrackingInterfaces.QueryExpand.Wiql)
 
     if (query === null) {
-        console.error(`query ${queryName} not found`);
-        process.exit(-1);
+        console.error(`query ${queryName} not found`)
+        process.exit(-1)
     }
-    return query;
+    return query
 }
 
 async function getTopLevelItems(witApi: WorkItemTrackingApi.IWorkItemTrackingApi, query: WorkItemTrackingInterfaces.QueryHierarchyItem) {
-    common.heading('Executing Query');
+    common.heading('Executing Query')
 
     if (query.queryType !== WorkItemTrackingInterfaces.QueryType.OneHop) {
-        console.error("Error: Only support tree based queries.");
+        console.error("Error: Only support tree based queries.")
 
-        throw "Query type not supported";
+        throw "Query type not supported"
     }
 
-    const queryResults = witApi.queryById(query.id);
+    const queryResults = witApi.queryById(query.id)
 
     return (await queryResults).workItemRelations
         .filter(wirel => wirel.rel == null && wirel.source == null)
-        .sort((a, b) => a.target.id - b.target.id);
+        .sort((a, b) => a.target.id - b.target.id)
 }
 
 export async function run(organizationUrl: string, projectName: string, queryName: string, relationType: string[], keepOriginalComment: boolean) : Promise<void> {
 
-    const webApi: nodeApi.WebApi = await common.getWebApi(organizationUrl);
+    const webApi: nodeApi.WebApi = await common.getWebApi(organizationUrl)
 
-    const witApi: WorkItemTrackingApi.IWorkItemTrackingApi = await webApi.getWorkItemTrackingApi();
-    const coreApiObject: CoreApi.CoreApi = await webApi.getCoreApi();
-    const project: CoreInterfaces.TeamProject = await coreApiObject.getProject(projectName);
+    const witApi: WorkItemTrackingApi.IWorkItemTrackingApi = await webApi.getWorkItemTrackingApi()
+    const coreApiObject: CoreApi.CoreApi = await webApi.getCoreApi()
+    const project: CoreInterfaces.TeamProject = await coreApiObject.getProject(projectName)
 
-    const query = await getQuery(witApi, project, queryName);
+    const query = await getQuery(witApi, project, queryName)
 
-    const topLevelWorkItems = await getTopLevelItems(witApi, query);
+    const topLevelWorkItems = await getTopLevelItems(witApi, query)
 
     if (topLevelWorkItems.length === 0) {
-        console.log(Colors.yellow("Query returned no results"));
+        console.log(Colors.yellow("Query returned no results"))
     } else {
-        common.result(`Query returned ${topLevelWorkItems.length} top level items.`);
+        common.result(`Query returned ${topLevelWorkItems.length} top level items.`)
 
-        let numberWorkItemsUpdated = 0;
-        let numberLinksUpdated = 0;
+        let numberWorkItemsUpdated = 0
+        let numberLinksUpdated = 0
         for (const item in topLevelWorkItems) {
 
-            const workItemId = topLevelWorkItems[item].target.id;
+            const workItemId = topLevelWorkItems[item].target.id
 
-            let jsonPatch: JsonPatchDocument;
-            const operations: Array<JsonPatchOperation> = [];
+            let jsonPatch: JsonPatchDocument
+            const operations: Array<JsonPatchOperation> = []
 
-            const workItem = await witApi.getWorkItem(workItemId, null, null, WorkItemTrackingInterfaces.WorkItemExpand.Relations);
+            const workItem = await witApi.getWorkItem(workItemId, null, null, WorkItemTrackingInterfaces.WorkItemExpand.Relations)
 
-            common.heading(`Examining ${workItemId} ${workItem.fields["System.Title"]}`);
+            common.heading(`Examining ${workItemId} ${workItem.fields["System.Title"]}`)
 
-            let relationCounter = 0;
-            let numberChanges = 0;
+            let relationCounter = 0
+            let numberChanges = 0
             for (const rel in workItem.relations) {
-                const relation = workItem.relations[rel];
+                const relation = workItem.relations[rel]
 
                 if (relationType.includes(relation.rel)) {
 
-                    let newComment = `Migration: original link type ${relation.attributes['name']}.`;
+                    let newComment = `Migration: original link type ${relation.attributes['name']}.`
 
                     if (keepOriginalComment && relation.attributes['comment']) {
-                        newComment += ` Original comment: ${relation.attributes['comment']}`;
+                        newComment += ` Original comment: ${relation.attributes['comment']}`
                     }
 
                     const operationAdd: JsonPatchOperation = {
@@ -103,23 +103,23 @@ export async function run(organizationUrl: string, projectName: string, queryNam
                         path: `/relations/${relationCounter}`
                     }
 
-                    operations.push(operationAdd, operationRemove);
-                    numberChanges++;
+                    operations.push(operationAdd, operationRemove)
+                    numberChanges++
                 }
-                relationCounter++;
+                relationCounter++
             }
 
             if (operations.length > 0) {
-                jsonPatch = operations;
+                jsonPatch = operations
 
-                await witApi.updateWorkItem(null, jsonPatch, workItem.id);
-                numberWorkItemsUpdated++;
-                numberLinksUpdated += numberChanges;
-                common.result(Colors.green(`Updated ${numberChanges} link(s)`));
+                await witApi.updateWorkItem(null, jsonPatch, workItem.id)
+                numberWorkItemsUpdated++
+                numberLinksUpdated += numberChanges
+                common.result(Colors.green(`Updated ${numberChanges} link(s)`))
 
             }
         }
-        console.log(Colors.green(`Updated ${numberLinksUpdated} links in ${numberWorkItemsUpdated} work item(s).`));
+        console.log(Colors.green(`Updated ${numberLinksUpdated} links in ${numberWorkItemsUpdated} work item(s).`))
     }
 }
 
@@ -127,7 +127,7 @@ export async function run(organizationUrl: string, projectName: string, queryNam
 
 if (process.env["API_TOKEN"] === null) {
     console.error("You need to define an environment variable called API_TOKEN with your Personal access token (PAT)")
-    process.exit(-3);
+    process.exit(-3)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -148,7 +148,7 @@ const argv: any = yargs.options({ })
         default: false,
         description: 'keep original comment'
     })
-    .argv;
+    .argv
 
 
 run(argv.org, argv.project, argv.query, argv.relType, argv.keepOriginalComment)
--- src/common.ts
+++ src/common.ts
@@ -1,43 +1,43 @@
-import * as vm from "azure-devops-node-api";
-import * as lim from "azure-devops-node-api/interfaces/LocationsInterfaces";
+import * as vm from "azure-devops-node-api"
+import * as lim from "azure-devops-node-api/interfaces/LocationsInterfaces"
 
 function getEnv(name: string): string {
-    const val = process.env[name];
+    const val = process.env[name]
     if (!val) {
-        console.error(`${name} env var not set`);
-        process.exit(1);
+        console.error(`${name} env var not set`)
+        process.exit(1)
     }
-    return val;
+    return val
 }
 
 export async function getWebApi(serverUrl: string): Promise<vm.WebApi> {
     return new Promise<vm.WebApi>((resolve, reject) => {
         try {
-            const token = getEnv("API_TOKEN");
-            const authHandler = vm.getPersonalAccessTokenHandler(token);
-            const option = undefined;
+            const token = getEnv("API_TOKEN")
+            const authHandler = vm.getPersonalAccessTokenHandler(token)
+            const option = undefined
 
-            const vsts: vm.WebApi = new vm.WebApi(serverUrl, authHandler, option);
+            const vsts: vm.WebApi = new vm.WebApi(serverUrl, authHandler, option)
             vsts.connect()
                 .then((connData: lim.ConnectionData) => {
-                    console.log(`Hello ${connData.authenticatedUser.providerDisplayName} you are connected to ${serverUrl}`);
-                    resolve(vsts);
+                    console.log(`Hello ${connData.authenticatedUser.providerDisplayName} you are connected to ${serverUrl}`)
+                    resolve(vsts)
                 })
                 .catch((err) => {
                     console.error("failed connecting", err)
-                });
+                })
         }
         catch (err) {
-            reject(err);
+            reject(err)
         }
-    });
+    })
 }
 
 export function heading(title: string): void {
-    console.log();
-    console.log(`> ${title}`);
+    console.log()
+    console.log(`> ${title}`)
 }
 
 export function result(title: string): void {
-    console.log(`    ${title}`);
+    console.log(`    ${title}`)
 }
